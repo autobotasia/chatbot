a9409b3acb2cee1a6d7ab7c74bdc6c8fa355e5c6
Nguyen Tran Nhan <pocolomos@gmail.com>
2020-04-07 10:51:17 +0700

    add skypeboot

---
+++ skypebot/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 443
+    APP_ID = "da68e4d7-702c-4587-a16a-619179db0f26"
+    APP_PASSWORD = "wt.@r4wdXMT:m6H/yps8hHfiPfVNqzV5"
